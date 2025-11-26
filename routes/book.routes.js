780f898702ec014270e24359e302098e135f1606
Sphinx <git config --global user.email {ID}+{username}@users.noreply.github.com>
2025-11-26 18:52:21 +0530

    Update

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -2,7 +2,7 @@ const express=require('express')
 const bookcontroller=require('../controller/book.controller')
 const router = express.Router()
 
-router.get('/',bookcontroller.getallboooks)
+router.get('/',bookcontroller.getallboooks) 
 router.get('/:id',bookcontroller.getbookbyid)
 router.post('/',bookcontroller.postbook) 
 router.delete('/:id',bookcontroller.d)
